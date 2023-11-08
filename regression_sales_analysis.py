caa473451e972f58cac255b5d4197ad0e8b08af1
milalangone <mlangone@itba.edu.ar>
2023-11-07 23:34:15 -0300

    ej 2 .pynb

--- regression_sales_analysis.py
+++
@@ -1,6 +0,0 @@
-## Analizar correlación entre las variables
-## Realizar los modelos de regresiónnn lineal simple
-## Realizar el modelo de regresión lineal múltiple
-## Realizar diagnóstico del modelo
-### Analizar la influencia de cada variable en las ventas
-## Dividir el conjunto total en unn conjunto de entrenamiento y otro de prueba y calcular las matrices de confusión, accuracy, precision y recall
